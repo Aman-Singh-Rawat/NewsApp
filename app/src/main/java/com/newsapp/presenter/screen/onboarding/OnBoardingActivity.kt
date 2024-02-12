dafe8a24b10e3403a318e4ac06386ff7d5ca1a7f
NAV0101 <pokhriyalnaveen1@gmail.com>
2024-02-13 01:18:50 +0530

    Splash screen added.

---
+++ app/src/main/java/com/newsapp/presenter/screen/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package com.newsapp.presenter.screen.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.newsapp.R
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
