9482353ca3d7454ef08382f85bb190fbcc6d270d
Joshi2001 <shubhamjoshi12050@gmail.com>
2024-02-13 16:54:19 +0530

    finial layout of Create_Account

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
---
+++ app/src/main/java/com/newsapp/presenter/screen/auth/All_Set_Fragment.kt
@@ -0,0 +1,19 @@
+package com.newsapp.presenter.screen.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.newsapp.R
+
+class All_Set_Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all__set_, container, false)
+    }
+}
