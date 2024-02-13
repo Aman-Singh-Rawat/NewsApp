948dbd7bc442ef172a8fdfb64b6960872f4ac038
Joshi2001 <shubhamjoshi12050@gmail.com>
2024-02-13 15:09:47 +0530

    first layout of Create_Account

---
+++ app/src/main/java/com/newsapp/presenter/screen/auth/SignUp.kt
@@ -0,0 +1,13 @@
+package com.newsapp.presenter.screen.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.newsapp.R
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+
+}
