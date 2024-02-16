431425c78a362382ea3522fbce427a98d867b1fc
aman <amanrawat6767@gmail.com>
2024-02-16 23:03:02 +0530

    fix the onBackPressed

--- app/src/main/java/com/newsapp/presenter/screen/profile/StoryPublishedFragment.kt
+++ app/src/main/java/com/newsapp/presenter/screen/profile/StoryPublishedFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.newsapp.R
 import com.newsapp.databinding.FragmentStoryPublishedBinding
 
@@ -17,7 +18,15 @@ class StoryPublishedFragment : Fragment() {
         binding = FragmentStoryPublishedBinding.inflate(
             inflater, container, false
         )
+        binding.imgStoryBackArrow.setOnClickListener {
+            onBackPressed()
+        }
         return binding.root
+    }
+    val onBackPressed = {
+        findNavController()
+            .navigateUp()
+        true
     }
 
 }
