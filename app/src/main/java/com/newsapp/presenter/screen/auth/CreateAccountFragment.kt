f939fb6f43a802cb39780cc71cce3e69e0da1e32
Joshi2001 <shubhamjoshi12050@gmail.com>
2024-02-17 14:45:11 +0530

    Add commmon layout

--- app/src/main/java/com/newsapp/presenter/screen/auth/CreateAccountFragment.kt
+++ app/src/main/java/com/newsapp/presenter/screen/auth/CreateAccountFragment.kt
@@ -19,7 +19,7 @@ class CreateAccountFragment : Fragment() {
         binding = FragmentCreateAccountBinding
             .inflate(inflater, container, false)
 
-        binding.btnSignUp.setOnClickListener {
+        binding.btn.setOnClickListener {
             openNewsFeed()
         }
         binding.ivBackArrowCreate.setOnClickListener { // this function work on back button
