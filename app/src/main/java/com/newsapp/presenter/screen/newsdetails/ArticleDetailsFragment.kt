c40efca76a021223aec5c8ee075a4f1a358b9d0e
Joshi2001 <shubhamjoshi12050@gmail.com>
2024-04-05 22:21:27 +0530

    Article details fragment ui update

--- app/src/main/java/com/newsapp/presenter/screen/newsdetails/ArticleDetailsFragment.kt
+++ app/src/main/java/com/newsapp/presenter/screen/newsdetails/ArticleDetailsFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.newsapp.R
 import com.newsapp.core.base.BaseFragment
 import com.newsapp.databinding.FragmentArticleDetailsBinding
 import com.newsapp.presenter.viewmodel.ArticleDetailViewModel
@@ -13,6 +16,8 @@ import com.newsapp.util.glideImage
 
 class ArticleDetailsFragment: BaseFragment() {
     private lateinit var binding: FragmentArticleDetailsBinding
+    private val newsAdapter: NewsAdapter = NewsAdapter()
+    private val commentAdapter: CommentAdapter = CommentAdapter()
     private val viewModel by activityViewModels<ArticleDetailViewModel>()
     private val articleId by lazy {arguments?.getString("articleId") ?: ""}
     override fun onCreateView(
@@ -26,8 +31,23 @@ class ArticleDetailsFragment: BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.tvCommentViewAll.setOnClickListener {
+            findNavController().navigate(R.id.commentFragment)
+        }
+        binding.ivbackArrow.setOnClickListener {
+            findNavController().navigateUp()
+        }
+        binding.ivBookMark.setOnClickListener {
+            findNavController().navigate(R.id.bookMarkBottomSheetFragment)
+        }
+
         firebaseSetup()
-        binding.rvNewsTags.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        rvCommentSetup()
+        binding.rvNewsTags.setHasFixedSize(true)
+        binding.rvNewsTags.adapter = newsAdapter
+        binding.rvNewsTags.layoutManager =
+            StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.HORIZONTAL)
+
     }
 
     private fun firebaseSetup() {
@@ -59,4 +79,48 @@ class ArticleDetailsFragment: BaseFragment() {
         }
     }
 
+    private fun rvCommentSetup() {
+        binding.rvComment.layoutManager =
+            LinearLayoutManager(
+                requireContext(), LinearLayoutManager.VERTICAL,
+                false
+            )
+        binding.rvComment.adapter = commentAdapter
+        commentAdapter.updateUi(getComment())
+    }
+
+    private fun getdata(): List<String> {
+        return listOf(
+            "politics", "corruption", "investiagtion", "crime", "government", "report", "cnn"
+        )
+    }
+
+    private fun getComment(): List<CommentData> {
+        return listOf(
+            CommentData(
+                R.drawable.img_girl_profile,
+                "Sanjuanita Ordonez",
+                "3 day ago",
+                "This investigative report is a powerful reminder of the importance of transparency and accountability in our political system.",
+                "256"
+            ),
+            CommentData(
+                R.drawable.imp_person_one,
+                "Sanju Ordonez",
+                "2 day ago",
+                "This investigative report is a powerful reminder of the importance of transparency and accountability in our political system.",
+                "36"
+            ),
+            CommentData(
+                R.drawable.img_girl_profile,
+                "anita Ordonez",
+                "1 day ago",
+                "This investigative report is a powerful reminder of the importance of transparency and accountability in our political system.",
+                "144"
+            )
+
+        )
+
+
+    }
 }
